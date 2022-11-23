8cf9eaac8641552cdcbcfdacaae0b7c305a6ac2f
aleonchik <aleonchik@gmail.com>
2022-11-23 20:46:50 +1000

    Lesson 86

--- src/main/java/ru/leonchik/city/dao/PersonCheckDao.java
+++ src/main/java/ru/leonchik/city/dao/PersonCheckDao.java
@@ -10,7 +10,13 @@ import java.sql.ResultSet;
 import java.sql.SQLException;
 
 public class PersonCheckDao {
-    private static final String SQL_REQUEST = "";
+    private static final String SQL_REQUEST = "SELECT temporal FROM cr_address_person ap " +
+            "    INNER JOIN cr_person p ON p.person_id = ap.person_id " +
+            "    INNER JOIN cr_address a ON a.address_id = ap.address_id " +
+            "    WHERE upper(p.sur_name) = upper(?) AND upper(p.given_name) = upper(?) " +
+            "        AND upper(p.patronymic) = upper(?) and p.date_of_birth = ? " +
+            "        AND a.street_code = ? " +
+            "        AND upper(a.building) = upper(?) and upper(a.extension) = upper(?) and upper(a.apartment) = upper(?)";
 
     public PersonResponse checkPerson(PersonRequest request) throws PersonCheckException {
         PersonResponse response = new PersonResponse();
--- src/main/resources/register.sql
+++ src/main/resources/register.sql
@@ -89,3 +89,25 @@ INSERT INTO cr_address_person (address_id, person_id, start_date,end_date)
 
 INSERT INTO cr_address_person (address_id, person_id, start_date,end_date)
     VALUES (1, 4, '2018-12-11', null);
+
+--SELECT temporal, upper(p.sur_name), upper('Васильев' COLLATE "en_US") FROM cr_address_person ap
+--    INNER JOIN cr_person p ON p.person_id = ap.person_id
+--    INNER JOIN cr_address a ON a.address_id = ap.address_id
+--    WHERE p.sur_name = 'Васильев' AND p.given_name = 'Павел' AND p.patronymic = 'Николаевич' and p.date_of_birth = '1995-03-18'
+--        AND a.street_code = 1 AND a.building = '10' and a.extension = '2' and a.apartment = '121';
+
+--SELECT temporal FROM cr_address_person ap
+--    INNER JOIN cr_person p ON p.person_id = ap.person_id
+--    INNER JOIN cr_address a ON a.address_id = ap.address_id
+--    WHERE upper(p.sur_name) = upper('Васильев') AND upper(p.given_name) = upper('Павел')
+--        AND upper(p.patronymic) = upper('Николаевич') and p.date_of_birth = '1995-03-18'
+--        AND a.street_code = 1
+--        AND upper(a.building) = upper('10') and upper(a.extension) = upper('2') and upper(a.apartment) = upper('121');
+
+SELECT temporal FROM cr_address_person ap
+    INNER JOIN cr_person p ON p.person_id = ap.person_id
+    INNER JOIN cr_address a ON a.address_id = ap.address_id
+    WHERE upper(p.sur_name) = upper('Васильев') AND upper(p.given_name) = upper('Павел')
+        AND upper(p.patronymic) = upper('Николаевич') and p.date_of_birth = '1995-03-18'
+        AND a.street_code = 1
+        AND upper(a.building) = upper('10') and upper(a.extension) = upper('2') and upper(a.apartment) = upper('121');
