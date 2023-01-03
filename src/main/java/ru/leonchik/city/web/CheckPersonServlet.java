4cae6cd72203dc156e7dcbdf745a7286b579a9d1
aleonchik <aleonchik@gmail.com>
2023-01-04 02:11:25 +1000

    Work with jakarta and tcat 10

--- src/main/java/ru/leonchik/city/web/CheckPersonServlet.java
+++ src/main/java/ru/leonchik/city/web/CheckPersonServlet.java
@@ -2,17 +2,18 @@ package ru.leonchik.city.web;
 
 import java.io.*;
 
-//import jakarta.servlet.ServletException;
-//import jakarta.servlet.http.HttpServlet;
-//import jakarta.servlet.http.HttpServletRequest;
-//import jakarta.servlet.http.HttpServletResponse;
+import jakarta.servlet.ServletException;
+import jakarta.servlet.annotation.WebServlet;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
 
 
-import javax.servlet.ServletException;
+/*import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
-import javax.servlet.http.HttpServletResponse;
+import javax.servlet.http.HttpServletResponse;*/
 import java.io.IOException;
 
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/chp"})
