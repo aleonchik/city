9b9347b1cb592e762f7d9e7cc5cdbc1c394fe822
aleonchik <aleonchik@gmail.com>
2023-01-04 01:38:45 +1000

    TCAT-9 in tcat-10 jakarta (not work)

---
+++ src/main/java/ru/leonchik/city/web/CheckPersonServlet.java
@@ -0,0 +1,26 @@
+package ru.leonchik.city.web;
+
+import java.io.*;
+
+//import jakarta.servlet.ServletException;
+//import jakarta.servlet.http.HttpServlet;
+//import jakarta.servlet.http.HttpServletRequest;
+//import jakarta.servlet.http.HttpServletResponse;
+
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import java.io.IOException;
+
+@WebServlet(name = "CheckPersonServlet", urlPatterns = {"/chp"})
+public class CheckPersonServlet extends HttpServlet {
+
+    @Override
+    protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+//        super.doGet(req, resp);
+        resp.getWriter().println("GET CheckPerson - called");
+    }
+}
