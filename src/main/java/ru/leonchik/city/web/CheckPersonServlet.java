dc64f5b2f352350bd11b23664677274aa9a8653b
aleonchik <aleonchik@gmail.com>
2023-01-04 13:35:28 +1000

    Lesson 100 Life cykle servlet

--- src/main/java/ru/leonchik/city/web/CheckPersonServlet.java
+++ src/main/java/ru/leonchik/city/web/CheckPersonServlet.java
@@ -1,27 +1,43 @@
 package ru.leonchik.city.web;
 
-import java.io.*;
-
+// import java.io.*;
 import jakarta.servlet.ServletException;
 import jakarta.servlet.annotation.WebServlet;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import ru.leonchik.city.dao.PersonCheckDao;
 import ru.leonchik.city.domain.PersonRequest;
 import ru.leonchik.city.domain.PersonResponse;
-
+import java.io.IOException;
+import java.time.LocalDate;
 
 /*import javax.servlet.ServletException;
 import javax.servlet.annotation.WebServlet;
 import javax.servlet.http.HttpServlet;
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;*/
-import java.io.IOException;
-import java.time.LocalDate;
 
+/**
+ * 1. Init
+ * 2. Service
+ * 3. Destroy
+ */
+
+//@WebServlet(name = "CheckPersonServlet", urlPatterns = {"/chp"}, loadOnStartup = 1)
 @WebServlet(name = "CheckPersonServlet", urlPatterns = {"/chp"})
 public class CheckPersonServlet extends HttpServlet {
+    private static final Logger logger = LoggerFactory.getLogger(CheckPersonServlet.class);
+    private PersonCheckDao dao;
+
+    @Override
+    public void init() throws ServletException {
+//        super.init();
+        logger.info("Servlet is created");
+        dao = new PersonCheckDao();
+    }
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
@@ -43,7 +59,7 @@ public class CheckPersonServlet extends HttpServlet {
         pr.setApartment("121");
 
         try {
-            PersonCheckDao dao = new PersonCheckDao();
+//            PersonCheckDao dao = new PersonCheckDao();
             PersonResponse ps = dao.checkPerson(pr);
 
             if (ps.isRegistered()) {
