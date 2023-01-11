de7a43badccbda4e60aec914a8c99b8ec148e774
aleonchik <aleonchik@gmail.com>
2023-01-11 14:02:43 +1000

    Lessn 104 ReST JSON Produces

--- src/main/java/ru/leonchik/city/web/CheckPersonService.java
+++ src/main/java/ru/leonchik/city/web/CheckPersonService.java
@@ -1,12 +1,31 @@
 package ru.leonchik.city.web;
 
-import jakarta.ws.rs.GET;
-import jakarta.ws.rs.Path;
+import jakarta.ws.rs.*;
+import jakarta.ws.rs.core.MediaType;
+import ru.leonchik.city.domain.PersonResponse;
+
+/**
+ * http://localhost:8080/city-register-1.0/rest/check/155?name=K155
+ * SimpleId = 155 : simpleName = K155
+ */
+
+/*@Path("/check")
+public class CheckPersonService {
+    @GET
+    @Path("/{id}")
+    public String checkPerson(@PathParam("id") int simpleId,
+                              @QueryParam("name") String simpleName) {
+        return "SimpleId = " + simpleId + " : " + "simpleName = " + simpleName;
+    }
+}*/
 
 @Path("/check")
 public class CheckPersonService {
     @GET
-    public String checkPerson() {
-        return "Simple STRING";
+    @Path("/{id}")
+    @Produces(MediaType.APPLICATION_JSON)
+    public PersonResponse checkPerson(@PathParam("id") int simpleId,
+                                      @QueryParam("name") String simpleName) {
+        return new PersonResponse();
     }
 }
