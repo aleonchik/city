e915a08c1d5214b3ce9b01afa5b1180d097e4ada
aleonchik <aleonchik@gmail.com>
2023-01-11 18:31:41 +1000

    Lesson-105 Rest JSON POST

--- src/main/java/ru/leonchik/city/web/CheckPersonService.java
+++ src/main/java/ru/leonchik/city/web/CheckPersonService.java
@@ -2,7 +2,10 @@ package ru.leonchik.city.web;
 
 import jakarta.ws.rs.*;
 import jakarta.ws.rs.core.MediaType;
+import ru.leonchik.city.domain.PersonRequest;
 import ru.leonchik.city.domain.PersonResponse;
+
+import java.time.LocalDate;
 
 /**
  * http://localhost:8080/city-register-1.0/rest/check/155?name=K155
@@ -19,7 +22,7 @@ public class CheckPersonService {
     }
 }*/
 
-@Path("/check")
+/*@Path("/check")
 public class CheckPersonService {
     @GET
     @Path("/{id}")
@@ -27,5 +30,52 @@ public class CheckPersonService {
     public PersonResponse checkPerson(@PathParam("id") int simpleId,
                                       @QueryParam("name") String simpleName) {
         return new PersonResponse();
-    }
+    }*/
+
+    /*@Path("/check")
+    public class CheckPersonService {
+        @GET
+        @Produces(MediaType.APPLICATION_JSON)
+        public PersonRequest checkPerson() {
+            PersonRequest pr = new PersonRequest();
+
+            pr.setSurName("Васильева");
+            pr.setGivenName("Ирина");
+            pr.setPatronymic("Петровна");
+            pr.setDateOfBirth(LocalDate.of(1997, 8, 21));
+            pr.setStreetCode(1);
+            pr.setBuilding("271");
+            pr.setExtension("2");
+            pr.setApartment("4");
+
+            return pr;
+        }*/
+/*
+URL http://localhost:8080/city-register-1.0/rest/check
+
+In postman extension (Chrome)
+Header Content-Type application/json
+
+{
+  "apartment": "4",
+  "building": "271",
+  "dateOfBirth": "21.08.1997",
+  "extension": "2",
+  "givenName": "Ирина",
+  "patronymic": "Петровна",
+  "streetCode": 1,
+  "surName": "Васильева"
+}
+ */
+
+
+    @Path("/check")
+    public class CheckPersonService {
+        @POST
+        @Consumes(MediaType.APPLICATION_JSON)
+        @Produces(MediaType.APPLICATION_JSON)
+        public PersonResponse checkPerson(PersonRequest request) {
+            System.out.println(request.toString());
+            return new PersonResponse();
+        }
 }
